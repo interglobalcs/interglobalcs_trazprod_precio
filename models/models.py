3d6474e269b12052caf91495f4187f752456a8f9
Oscar Aponte <desarrollo@interglobalcs.com>
2018-03-09 10:36:29 -0600

    Commit de salida

--- models/models.py
+++ models/models.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
 
 class igcs_apr_prod(models.Model):
     _inherit = 'product.template'
 
-    website_published = fields.Boolean('Visible in Website', copy=False, track_visibility='always')
+    website_published = fields.Boolean('Visible in Website', copy=False, track_visibility='onchange')
+    list_price = fields.Float(
+        'Sales Price', default=1.0,
+        digits=dp.get_precision('Product Price'),
+        help="Base price to compute the customer price. Sometimes called the catalog price.", track_visibility='onchange')
